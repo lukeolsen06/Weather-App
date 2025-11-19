eb209496e5b9698c1c69fa321b59240afa93c37d
LO-2002-2024 <123707233+lukeolsen06@users.noreply.github.com>
2025-11-19 18:32:23 -0500

    chore: renamed files and organized directory

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/weather/weather.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
