c2c76c49fa300ccab8a5c8399a34dc3e25a88360
LO-2002-2024 <123707233+lukeolsen06@users.noreply.github.com>
2025-11-20 13:51:54 -0500

    feat: created a small scaffold of frontend with search bar component, weather card component, and tailwind installed

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+
+const SearchBar = () => {
+
+  const [searchLocation, setSearchLocation] = useState('')
+
+  const searchForWeather = () => {
+    setSearchLocation(searchLocation)
+    // const data = await axios.get()
+  }
+
+  return (
+    <div>
+        <div className="flex justify-center items-center mt-6 gap-2 w-full">
+          <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-blue-600">
+            <input id="location" type="text" placeholder="Enter a city name, coordinates, or zip code..." className="w-80 sm:w-[28rem] grow bg-white py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+          </div>
+          <button type="button" className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50" onClick={searchForWeather}>Search</button>
+        </div>
+    </div>
+  )
+}
+
+export default SearchBar
---
+++ frontend/src/components/WeatherCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+// Will receive props containing JSON weather response object
+const WeatherCard = ({}) => {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default WeatherCard
