8ac6a4155e931e77342f988e8a58d633ea2a55fb
LO-2002-2024 <123707233+lukeolsen06@users.noreply.github.com>
2025-11-21 09:32:14 -0500

    feat: Implemented ThrottlerGuard for rate-limiting

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,26 +4,50 @@ import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './modules/weather/weather.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { createKeyv } from '@keyv/redis';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
 
     imports: [
-    // Configuration module - loads environment variables
+        // Configuration module - loads environment variables
         ConfigModule.forRoot({
             isGlobal: true, // Makes config available everywhere
             envFilePath: ['.env.local', '.env'], // Loads .env files in order
             ignoreEnvFile: false, // Don't ignore .env files
         }),
 
+        // Use Redis store as cache, default to in-memory
+        // TTL of 10 minutes
         CacheModule.registerAsync({
             useFactory: async () => ({
-                stores: [createKeyv('redis://localhost:6379')],
-                ttl: 15 * 60 * 1000 
+                stores: [createKeyv('redis://localhost:6379')], //This will be the Redis URI in production
+                ttl: 10 * 60 * 1000 
             }),
             isGlobal: true
         }),
 
+        // Rate-limiting factor
+        ThrottlerModule.forRoot({
+            throttlers: [
+                {
+                    ttl: 60000,
+                    limit: 25
+                }
+            ]
+        }),
+       
         WeatherModule
+    
+    ],
+    
+    providers: [ 
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard
+        }
     ]
+
+
 })
 export class AppModule {}
